ab82bbff3329c5290dea1d2b13234dac46ce6313
Neeraj Chandel <34812602+Neeraj224@users.noreply.github.com>
2024-09-27 02:24:47 -0400

    Update functions.kt

--- 1-kotlin-semantics/functions.kt
+++ 1-kotlin-semantics/functions.kt
@@ -27,9 +27,29 @@ fun main() {
     var emailID = "sample@gmail.com"
     
     println(displayAlertMessage(OS, emailID))
-    println(displayAlertMessage("Windows", emailID))
-    println(displayAlertMessage(emailID = emailID))
-    println(displayAlertMessage("Mac OS", emailID))
+    println()
+    //println(displayAlertMessage("Windows", emailID))
+    //println(displayAlertMessage(emailID = emailID))
+    //println(displayAlertMessage("Mac OS", emailID))
+    
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailID = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
 fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailID: String): String {

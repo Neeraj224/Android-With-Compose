674e1a0a3163defc4052853dab8d0f0dd0bf6aeb
Neeraj Chandel <34812602+Neeraj224@users.noreply.github.com>
2024-09-29 14:31:44 -0400

    Update MainActivity.kt

--- 2-apps/Card/app/src/main/java/com/example/birthday/MainActivity.kt
+++ 2-apps/Card/app/src/main/java/com/example/birthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -22,11 +23,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthday.ui.theme.BirthdayTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,9 +44,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
 //                    color = MaterialTheme.colorScheme.background
                 ) { innerPadding ->
-                    GreetingText(
-                        message = "Happy Birthday Sam!",
-                        from = "- from Emma",
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(id = R.string.signature_text),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -57,45 +63,89 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize()
     ) {
         Surface(
-            color = Color(red = 0.8f, green = 0.2f, blue = 0.3f, alpha = 0.4f),
-            shape = RoundedCornerShape(20.dp),
+            color = Color(red = 0.15f, green = 0.25f, blue = 0.39f, alpha = 0.1f),
+            shape = RoundedCornerShape(100.dp),
             modifier = modifier
-                .border(2.dp, Color.Black, RoundedCornerShape(20.dp))
                 .size(300.dp),
         ) {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillMaxSize()
             ) {
-                Column {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .border(
+                            width = 6.dp,
+                            color = Color(red = 0.2f, green = 0.1f, blue = 0.5f, alpha = 0.3f),
+                            shape = RoundedCornerShape(20.dp)
+                        )
+                ) {
                     Text(
                         text = message,
+                        color = Color(red = 0.2f, green = 0.1f, blue = 0.5f, alpha = 0.5f),
                         fontSize = 40.sp,
                         lineHeight = 40.sp,
-                        modifier = Modifier.padding(16.dp)
+                        modifier = Modifier.padding(24.dp)
                     )
                     Box(
                         modifier = Modifier.align(alignment = Alignment.End)
                     ) {
                         Text(
                             text = from,
+                            color = Color(red = 0.2f, green = 0.1f, blue = 0.5f, alpha = 0.5f),
                             fontSize = 30.sp,
                             modifier = Modifier.padding(16.dp)
                         )
                     }
-
                 }
-
             }
         }
     }
 }
 
+/**
+*   Modifiers: An ordered, immutable collection of modifier elements that decorate
+*   or add behavior to Compose UI elements. For example, backgrounds, padding
+*   and click event listeners decorate or add behavior to rows, text or buttons.
+* */
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    // the painterResource function loads a drawable image resource and takes resource ID
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box(modifier)
+    {
+        Image(
+            painter = image,
+            // we'll make the contentDescription null so that TalkBack skips it
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.8F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize()
+
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayPreview() {
     BirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "- from Emma")
+//        GreetingText(
+//            message = "Happy Birthday Sam!",
+//            from = "- from Emma"
+//        )
+        GreetingImage(
+            message = stringResource(id = R.string.happy_birthday_text),
+            from = stringResource(id = R.string.signature_text)
+        )
     }
 }
